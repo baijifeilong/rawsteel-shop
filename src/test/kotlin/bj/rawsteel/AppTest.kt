6dd6c82045f54a6a0d5fdf9f8fa06ab27c6cffa0
songjianwei <baijifeilong@163.com>
2018-07-03 17:22:38 +0800

    Abstract a parent test class

--- src/test/kotlin/bj/rawsteel/AppTest.kt
+++ src/test/kotlin/bj/rawsteel/AppTest.kt
@@ -1,12 +1,17 @@
 package bj.rawsteel
 
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
 
 
 /**
  * Created by BaiJiFeiLong@gmail.com at 2018/7/3 下午2:52
  */
-class AppTest {
+@RunWith(SpringRunner::class)
+@SpringBootTest(classes = [App::class])
+open class AppTest {
 
     @Test
     fun main() {
--- src/test/kotlin/bj/rawsteel/repository/UserRepositoryTest.kt
+++ src/test/kotlin/bj/rawsteel/repository/UserRepositoryTest.kt
@@ -1,18 +1,13 @@
 package bj.rawsteel.repository
 
-import bj.rawsteel.App
+import bj.rawsteel.AppTest
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
 import javax.annotation.Resource
 
 /**
  * Created by BaiJiFeiLong@gmail.com at 2018/7/3 下午5:08
  */
-@RunWith(SpringRunner::class)
-@SpringBootTest(classes = [(App::class)])
-class UserRepositoryTest {
+class UserRepositoryTest : AppTest() {
 
     @Resource
     private lateinit var userRepository: UserRepository
