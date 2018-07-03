8baec71cb53afcb1dd56fc121bd33f4e3cc6ea38
songjianwei <baijifeilong@163.com>
2018-07-03 15:25:09 +0800

    Add hello world http response

--- src/main/kotlin/bj/rawsteel/App.kt
+++ src/main/kotlin/bj/rawsteel/App.kt
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.ApplicationListener
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 /**
  * Created by BaiJiFeiLong@gmail.com at 2018/7/3 下午2:40
  */
 @SpringBootApplication
+@RestController
 open class App : ApplicationListener<ApplicationReadyEvent> {
     override fun onApplicationEvent(event: ApplicationReadyEvent?) {
         println("here")
@@ -19,5 +22,10 @@ open class App : ApplicationListener<ApplicationReadyEvent> {
         fun main(args: Array<String>) {
             SpringApplication.run(App::class.java, *args)
         }
+    }
+
+    @RequestMapping("/")
+    fun index(): Any {
+        return "Hello world!";
     }
 }
