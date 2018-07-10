4538c5b3642a3bb80e72784cfe03ed9641b02849
BaiJiFeiLong <baijifeilong@gmail.com>
2018-07-10 15:50:55 +0800

    Use history route rather than hash route; Add NotFound page

--- ui/src/router/index.ts
+++ ui/src/router/index.ts
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello.vue'
+import NotFound from '@/components/NotFound.vue'
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/404',
+      name: 'NotFound',
+      component: NotFound
     }
   ]
 })
