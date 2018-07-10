3755e07a85c9c81d9fcb89ddb8e54bc8165fec45
BaiJiFeiLong <baijifeilong@gmail.com>
2018-07-10 18:25:23 +0800

    Add vue-material; Add LESS support

--- ui/src/components/NotFound.vue
+++ ui/src/components/NotFound.vue
@@ -1,5 +1,16 @@
 <template>
-  <h1>404 Not Found</h1>
+  <div class="my-not-found">
+    <h1>404 Not Found</h1>
+    <div class="my-container">
+      <md-content class="my-box md-elevation-5" :key="item" v-for="item in [1,2,3,4,5]">{{item}}</md-content>
+    </div>
+
+    <div class="md-layout md-gutter">
+      <div class="md-layout-item">item</div>
+      <div class="md-layout-item">item</div>
+      <div class="md-layout-item">item</div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +19,29 @@
   }
 </script>
 
-<style scoped>
+<style scoped lang="less">
+
+  .my-not-found {
+    .my-container {
+      display: flex;
+      flex-wrap: wrap;
+      padding: 16px;
+      .my-box {
+        width: 100px;
+        height: 100px;
+        background: yellowgreen;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: 24px;
+      }
+    }
+  }
+
+  .md-layout-item {
+    border: 1px solid beige;
+    height: 120px;
+    background: aliceblue;
+  }
 
 </style>
