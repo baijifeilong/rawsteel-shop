dddb055b97c80e6dbd93031a06a18eb18b3a195e
BaiJiFeiLong <baijifeilong@gmail.com>
2018-07-11 21:24:54 +0800

    Fix null error when get user from localStorage

--- ui/src/common/globals.ts
+++ ui/src/common/globals.ts
@@ -1,11 +1,11 @@
 import User from "../domain/User";
 
 export function getToken(): string {
-  return (getUser() || new User).token
+  return getUser().token;
 }
 
 export function getUser(): User {
-  return JSON.parse(localStorage.getItem('user'))
+  return JSON.parse(localStorage.getItem('user')) || new User;
 }
 
 export function setUser(user: User): void {
