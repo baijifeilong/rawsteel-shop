7400503d3e38237d45cba928ad584ec6234be28a
BaiJiFeiLong <baijifeilong@gmail.com>
2018-07-17 14:54:40 +0800

    Combine api domains to same file

--- src/main/kotlin/bj/rawsteel/domain/Api.kt
+++ src/main/kotlin/bj/rawsteel/domain/Api.kt
@@ -3,8 +3,33 @@ package bj.rawsteel.domain
 import org.springframework.data.domain.Page
 
 /**
- * Created by BaiJiFeiLong@gmail.com at 2018/7/6 下午4:27
+ * Created by BaiJiFeiLong@gmail.com at 2018/7/6 下午4:25
  */
+class ApiSuccess<T> private constructor(var data: T) {
+
+    companion object {
+        fun <T> of(t: T): ApiSuccess<T> {
+            return ApiSuccess(t)
+        }
+    }
+}
+
+class ApiFailure private constructor(code: Int, message: String) {
+    var error: Error
+
+    init {
+        this.error = ApiFailure.Error(code, message)
+    }
+
+    companion object {
+        fun of(code: Int, message: String): ApiFailure {
+            return ApiFailure(code, message)
+        }
+    }
+
+    data class Error(val code: Int, val message: String)
+}
+
 data class ApiPage<T>(
         var pageIndex: Int,
         var itemsPerPage: Int,
